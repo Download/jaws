3953f0e53b0ade73a6c349a4c4ebd40c5ac05111
Stijn de Witt <StijnDeWitt@gmail.com>
2012-02-10 17:09:33 +0100

    Initial checkin

---
+++ jaws/src/main/java/com/github/download/jaws/http/Servlet.java
@@ -0,0 +1,21 @@
+package com.github.download.jaws.http;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // your logic here
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // your logic here
+    }
+
+}
