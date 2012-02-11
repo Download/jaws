d7cb0e1abd2ef04114054010ec8198a7f39f1e26
Stijn de Witt <StijnDeWitt@gmail.com>
2012-02-11 15:06:14 +0100

    Testing Eclipse <--> Github...

--- jaws/src/main/java/com/github/download/jaws/http/Servlet.java
+++ jaws/src/main/java/com/github/download/jaws/http/Servlet.java
@@ -11,11 +11,11 @@ public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // your logic here
+        //  your logic here
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // your logic here
+        //  your logic here
     }
 
 }
